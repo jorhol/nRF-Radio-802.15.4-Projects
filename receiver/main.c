58d44edc3ac3da2a65aaeeac60bdc6799bf2c75b
Holmefjord <jorgen.holmefjord@nordicsemi.no>
2019-05-08 16:39:00 +0200

    Add UART to transmit data wirelessly between two terminals

--- receiver/main.c
+++ receiver/main.c
@@ -3,7 +3,16 @@
 #include <string.h>
 
 #include "nrf_802154.h"
+#include "boards.h"
+#include "app_uart.h"
+#if defined (UART_PRESENT)
+#include "nrf_uart.h"
+#endif
+#if defined (UARTE_PRESENT)
+#include "nrf_uarte.h"
+#endif
 
+#define PACKET_HEADER_LENGTH 7
 #define MAX_MESSAGE_SIZE 127
 #define CHANNEL          11
 
@@ -11,10 +20,60 @@ static uint8_t m_message[MAX_MESSAGE_SIZE];
 
 static volatile uint32_t rx_counter;
 
+#define UART_TX_BUF_SIZE 256                         /**< UART TX buffer size. */
+#define UART_RX_BUF_SIZE 256                         /**< UART RX buffer size. */
+
+
+void uart_event_handle(app_uart_evt_t * p_event)
+{
+    if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR)
+    {
+        APP_ERROR_HANDLER(p_event->data.error_communication);
+    }
+    else if (p_event->evt_type == APP_UART_FIFO_ERROR)
+    {
+        APP_ERROR_HANDLER(p_event->data.error_code);
+    }
+}
+
+/**@brief  Function for initializing the UART module.
+ */
+/**@snippet [UART Initialization] */
+static void uart_init(void)
+{
+    uint32_t                     err_code;
+    app_uart_comm_params_t const comm_params =
+    {
+        .rx_pin_no    = RX_PIN_NUMBER,
+        .tx_pin_no    = TX_PIN_NUMBER,
+        .rts_pin_no   = RTS_PIN_NUMBER,
+        .cts_pin_no   = CTS_PIN_NUMBER,
+        .flow_control = APP_UART_FLOW_CONTROL_DISABLED,
+        .use_parity   = false,
+#if defined (UART_PRESENT)
+        .baud_rate    = NRF_UART_BAUDRATE_115200
+#else
+        .baud_rate    = NRF_UARTE_BAUDRATE_115200
+#endif
+    };
+
+    APP_UART_FIFO_INIT(&comm_params,
+                       UART_RX_BUF_SIZE,
+                       UART_TX_BUF_SIZE,
+                       uart_event_handle,
+                       APP_IRQ_PRIORITY_LOWEST,
+                       err_code);
+    APP_ERROR_CHECK(err_code);
+}
+/**@snippet [UART Initialization] */
+
+
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
+    
+    uart_init();
 
     uint8_t extended_address[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
     uint8_t short_address[]    = {0x05, 0x06};
@@ -37,7 +96,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void nrf_802154_received(uint8_t * p_data, uint8_t length, int8_t power, uint8_t lqi)
+void nrf_802154_received_timestamp(uint8_t * p_data, uint8_t length, int8_t power, uint8_t lqi, uint32_t time)
 {
     (void) power;
     (void) lqi;
@@ -46,8 +105,14 @@ void nrf_802154_received(uint8_t * p_data, uint8_t length, int8_t power, uint8_t
     {
         goto exit;
     }
-
-    memcpy(m_message, p_data, length);
+    {
+        memcpy(m_message, p_data, length);
+        uint32_t err_code;
+        uint8_t string_length = length - PACKET_HEADER_LENGTH - 2;
+        uint8_t received_string[string_length];
+        memcpy(received_string, p_data+PACKET_HEADER_LENGTH, string_length);
+        printf("t: %i, RSSI: %d, %s", time, power, received_string);
+    }
     rx_counter++;
 
 exit:
--- transmitter/main.c
+++ transmitter/main.c
@@ -1,24 +1,131 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "nrf_802154.h"
+#include "boards.h"
 #include "nrf_delay.h"
+#include "app_uart.h"
+#if defined (UART_PRESENT)
+#include "nrf_uart.h"
+#endif
+#if defined (UARTE_PRESENT)
+#include "nrf_uarte.h"
+#endif
 
-#define MAX_MESSAGE_SIZE 16
+#define PACKET_HEADER_LENGTH 7
+#define MAX_MESSAGE_SIZE 120
 #define CHANNEL          11
 
 static volatile bool m_tx_in_progress;
 static volatile bool m_tx_done;
+static uint8_t message[MAX_MESSAGE_SIZE];
+
+#define UART_TX_BUF_SIZE 256                         /**< UART TX buffer size. */
+#define UART_RX_BUF_SIZE 256                         /**< UART RX buffer size. */
+
+/**@brief   Function for handling app_uart events.**/
+void uart_event_handle(app_uart_evt_t * p_event)
+{
+    static uint8_t data_array[MAX_MESSAGE_SIZE];
+    static uint8_t index = 0;
+    uint32_t       err_code;
+
+    switch (p_event->evt_type)
+    {
+        case APP_UART_DATA_READY:
+            UNUSED_VARIABLE(app_uart_get(&data_array[index]));
+            index++;
+
+            if ((data_array[index - 1] == '\n') ||
+                (data_array[index - 1] == '\r'))
+            {
+                if (index > 1)
+                {
+                    uint8_t length = index < (MAX_MESSAGE_SIZE-PACKET_HEADER_LENGTH) ? index : (MAX_MESSAGE_SIZE-PACKET_HEADER_LENGTH);
+                    memcpy((message+PACKET_HEADER_LENGTH), data_array, length);
+                    if(PACKET_HEADER_LENGTH + index < MAX_MESSAGE_SIZE)
+                    {
+                        message[PACKET_HEADER_LENGTH+length] = '\0';
+                    }
+                    else
+                    {
+                        message[MAX_MESSAGE_SIZE-2] = '\n';
+                        message[MAX_MESSAGE_SIZE-1] = '\0';
+                    }
+                    do
+                    {
+                        
+                        if (!m_tx_in_progress)
+                        {
+                            m_tx_in_progress = nrf_802154_transmit(message, (PACKET_HEADER_LENGTH + length), true);
+                        }
+                        if (m_tx_done)
+                        {
+                            m_tx_in_progress = false;
+                            m_tx_done        = false;
+                        }
+                    } while (!m_tx_in_progress);
+                }
+                index = 0;
+            }
+            break;
+
+        case APP_UART_COMMUNICATION_ERROR:
+            APP_ERROR_HANDLER(p_event->data.error_communication);
+            break;
+
+        case APP_UART_FIFO_ERROR:
+            APP_ERROR_HANDLER(p_event->data.error_code);
+            break;
+
+        default:
+            break;
+    }
+}
+/**@snippet [Handling the data received over UART] */
+
+
+/**@brief  Function for initializing the UART module.
+ */
+/**@snippet [UART Initialization] */
+static void uart_init(void)
+{
+    uint32_t                     err_code;
+    app_uart_comm_params_t const comm_params =
+    {
+        .rx_pin_no    = RX_PIN_NUMBER,
+        .tx_pin_no    = TX_PIN_NUMBER,
+        .rts_pin_no   = RTS_PIN_NUMBER,
+        .cts_pin_no   = CTS_PIN_NUMBER,
+        .flow_control = APP_UART_FLOW_CONTROL_DISABLED,
+        .use_parity   = false,
+#if defined (UART_PRESENT)
+        .baud_rate    = NRF_UART_BAUDRATE_115200
+#else
+        .baud_rate    = NRF_UARTE_BAUDRATE_115200
+#endif
+    };
+
+    APP_UART_FIFO_INIT(&comm_params,
+                       UART_RX_BUF_SIZE,
+                       UART_TX_BUF_SIZE,
+                       uart_event_handle,
+                       APP_IRQ_PRIORITY_LOWEST,
+                       err_code);
+    APP_ERROR_CHECK(err_code);
+}
+/**@snippet [UART Initialization] */
 
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
 
-    uint8_t message[MAX_MESSAGE_SIZE];
+    uart_init(); 
 
-    for (uint32_t i = 0; i < sizeof(message) / sizeof(message[0]); i++)
+    for (uint32_t i = 0; i < PACKET_HEADER_LENGTH; i++)
     {
         message[i] = i;
     }
@@ -35,17 +142,7 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        if (m_tx_done)
-        {
-            m_tx_in_progress = false;
-            m_tx_done        = false;
-        }
-
-        if (!m_tx_in_progress)
-        {
-            m_tx_in_progress = nrf_802154_transmit(message, sizeof(message), true);
-        }
-        nrf_delay_ms(1000);
+        // Wait for UART
     }
 
     return 0;
